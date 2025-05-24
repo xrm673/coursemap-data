3193c588d25fdfb8818f058af54cc0e1e9b8ccdb
Raymond Xu <rx67@cornell.edu>
2025-05-23 21:25:21 -0400

    update constants to include FA25 and SU25 in the current year

--- const.py
+++ const.py
@@ -5,10 +5,10 @@ home_dir = str(Path.home())
 SERVICE_ACCOUNT_PATH = os.path.join(home_dir, '.config', 'firebase', 'service-account.json')
 
 CURRENT_YEAR = [
+    "FA25",
+    "SU25",
     "SP25",
     "WI25",
-    "FA24",
-    "SU24",
 ]
 
 ALL_SEMESTERS = [
